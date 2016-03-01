c32fb01699508553a20a378c77114cecca685212
IsemanTech <johnsoni1440@gmail.com>
2016-03-01 01:15:04 -0500

    increased the window size

--- pong.cpp
+++ pong.cpp
@@ -6,24 +6,24 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp> 
 
-void ball_traits(sf::Clock clock, sf::RectangleShape& ball, sf::RectangleShape leftPaddle);
+void ball_traits(sf::Clock clock, sf::RectangleShape& ball, sf::RectangleShape& leftPaddle, sf::RectangleShape& rightPaddle);
 
 int main()
 {
   sf::Clock clock;
-  const int gameWidth = 500;
-  const int gameHeight = 400;
+  const int gameWidth = 800;
+  const int gameHeight = 600;
   std::srand(time(0));
   sf::RenderWindow window(sf::VideoMode(gameWidth, gameHeight), "Pong!");
   window.setFramerateLimit(60); 
   //Create left paddle (player's paddle)
   sf::RectangleShape leftPaddle(sf::Vector2f(20, 100));
     leftPaddle.setFillColor(sf::Color::Green);
-    leftPaddle.setPosition(10, 10);
+    leftPaddle.setPosition(20, 10);
     //Create right paddle (AI)
     sf::RectangleShape rightPaddle(sf::Vector2f(20, 100));
     rightPaddle.setFillColor(sf::Color::Green);
-    rightPaddle.setPosition(470, 10);
+    rightPaddle.setPosition(760, 10);
     //Create the ball
     sf::RectangleShape ball(sf::Vector2f(20, 20));
     ball.setFillColor(sf::Color::Green);
@@ -40,21 +40,21 @@ int main()
         }
 	    //Player's Left Paddle movement
 	//Up
-	if ((sf::Keyboard::isKeyPressed(sf::Keyboard::W)) && leftPaddle.getPosition().y > gameHeight - 410)
+	if ((sf::Keyboard::isKeyPressed(sf::Keyboard::W)) && leftPaddle.getPosition().y > 0)
       {
 	//lets get the y axis values going up
-	//std::cout << "Position: "<<  leftPaddle.getPosition().y << std::endl; 
-	leftPaddle.move(0 , -.2);
+	//std::cout << "Position: "<<  leftPaddle.getPosition().y << std::endl;
+	leftPaddle.move(0 , -10);
       }
 	//Down
 	if ((sf::Keyboard::isKeyPressed(sf::Keyboard::S)) && leftPaddle.getPosition().y < gameHeight - 100)
       {
 	//let me get the y axis values going down
 	//std::cout << "Position: " << leftPaddle.getPosition().y << std::endl; 
-	leftPaddle.move(0, .2);
+	leftPaddle.move(0, 10);
       }
 	//Ball Physics
-	ball_traits(clock, ball, leftPaddle);
+	ball_traits(clock, ball, leftPaddle, rightPaddle);
 	//clear
         window.clear();
 	//draw
@@ -66,11 +66,13 @@ int main()
 
     return 0;
 }
-void ball_traits(sf::Clock clock, sf::RectangleShape& ball, sf::RectangleShape leftPaddle)
+void ball_traits(sf::Clock clock, sf::RectangleShape& ball, sf::RectangleShape& leftPaddle, sf::RectangleShape& rightPaddle)
 {
   double pi = 3.1415;
-    double ballSpeed = 10;
-    int angle = 30 * pi/180;
+  double ballSpeed = 30;
+    //convert angle to radians 
+    int angle = 0;
+      angle *= pi/180;
     //Scale X and Y will give the angle
     double scaleX = cos(angle);
     //std::cout << scaleX << std::endl;
@@ -82,16 +84,14 @@ void ball_traits(sf::Clock clock, sf::RectangleShape& ball, sf::RectangleShape l
     double moveY = ball.getPosition().y;
 	  
     double elapsed = clock.restart().asSeconds();
-    std::cout << elapsed << std::endl; 
-    //    while (ball.getPosition().x > 10 && ball.getPosition().y > 10)
-    // {
-	    moveX += velocityX  * elapsed;
-	    moveY += velocityY * elapsed;
-	    std::cout << moveX << std::endl;
-	    std::cout << moveY << std::endl;
-            ball.move(moveX,  moveY);
-	    ball.setPosition(moveX, moveY);
-	    //  }
-      
-	 
+    //std::cout << elapsed << std::endl; 
+	 moveX += velocityX  * elapsed;
+	moveY += velocityY * elapsed;
+	//std::cout << moveX << std::endl;
+	//std::cout << moveY << std::endl;
+       //set ball's new position
+	ball.setPosition(-moveX, moveY);
+       //prevent the ball from going out of bounds
+       std::cout << ball.getPosition().x << std::endl;
+       
 }
