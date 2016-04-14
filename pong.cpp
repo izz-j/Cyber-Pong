70855d8a4a50382bf660a2d3bc246e651b9112a0
mitem9106 <mitem9106@gmail.com>
2016-04-13 20:53:20 -0400

    Added the a score counter for both sides

--- pong.cpp
+++ pong.cpp
@@ -1,4 +1,4 @@
-//Authors: Iseman Johnson 
+//Authors: Iseman Johnson
 //First Pong game
 #include <iostream>
 #include <math.h>//for sine and cosine
@@ -7,9 +7,9 @@
 #include <SFML/Audio.hpp> //for sound
 
   float pi = 3.1415;
-void ball_traits(sf::RectangleShape& ball, sf::RectangleShape& leftPaddle, sf::RectangleShape& rightPaddle, int& direction, sf::Clock& clock, float& angle, sf::Sound& blip);
+void ball_traits(sf::RectangleShape& ball, sf::RectangleShape& leftPaddle, sf::RectangleShape& rightPaddle, int& direction, sf::Clock& clock, float& angle, sf::Sound& blip, int& scoreLeft, int& scoreRight, std::string& left, std::string& right, sf::Text& textLeft, sf::Text& textRight);
 int paddle_collision(const int& paddleSizeY, sf::RectangleShape& ball, sf::RectangleShape& leftPaddle, sf::RectangleShape& rightPaddle, int& direction, float& angle, sf::Sound& blip);
-void ai_paddle(sf::RectangleShape& ball, sf::RectangleShape& rightPaddle,  int& direction, sf::Clock& clock, const int& gameHeight); 
+void ai_paddle(sf::RectangleShape& ball, sf::RectangleShape& rightPaddle,  int& direction, sf::Clock& clock, const int& gameHeight);
 
 int main()
 {
@@ -18,11 +18,16 @@ int direction = -1;
   const int gameWidth = 800;
   const int gameHeight = 600;
   std::srand(time(0));
+  // initialized with 00 as a placeholder
+  std::string left = "00";
+  std::string right = "00";
+  int scoreLeft = 0;
+  int scoreRight = 0;
   sf::RenderWindow window(sf::VideoMode(gameWidth, gameHeight), "Pong!");
   window.setFramerateLimit(65);
   //Create left paddle (player's paddle)
    const int paddleSizeY = 100;
-  sf::RectangleShape leftPaddle(sf::Vector2f(20, paddleSizeY));
+    sf::RectangleShape leftPaddle(sf::Vector2f(20, paddleSizeY));
     leftPaddle.setFillColor(sf::Color::Green);
     leftPaddle.setPosition(20, 200);
     //Create right paddle (AI)
@@ -33,15 +38,42 @@ int direction = -1;
     sf::RectangleShape ball(sf::Vector2f(20, 20));
     ball.setFillColor(sf::Color::Green);
     ball.setPosition(250, 200);
+    //Create the line down the middle
+    sf::RectangleShape line(sf::Vector2f(4, 600));
+    line.setFillColor(sf::Color::White);
+    line.setPosition(399, 0);
     //ball initial angle
      float angle = 0;
      //load sound
      sf::SoundBuffer blipBuffer;
-     if (!blipBuffer.loadFromFile("audio/pongblip.wav"))
-	 return -1;
+     if (!blipBuffer.loadFromFile("C:/Users/mitem9106/Documents/GitHub/audio/pongblip.wav"))
+        return -1;
      sf::Sound blip;
      blip.setBuffer(blipBuffer);
     //This is the game loop gameplay goes in here
+
+    //Load font
+    sf::Font font;
+    if(!font.loadFromFile("C:/Users/mitem9106/Documents/GitHub/font/HATTEN.TTF"))
+        return -1;
+    //creating text objects and giving them parameters
+    sf::Text textLeft;
+    sf::Text textRight;
+    textLeft.setFont(font);
+    textRight.setFont(font);
+
+    textLeft.setString((left[1]));
+    textRight.setString((right[1]));
+
+    textLeft.setCharacterSize(100);
+    textRight.setCharacterSize(100);
+
+    textLeft.setColor(sf::Color::Red);
+    textRight.setColor(sf::Color::Red);
+
+    textLeft.setPosition(200, 200);
+    textRight.setPosition(570, 200);
+
     while (window.isOpen())
     {
         sf::Event event;
@@ -66,7 +98,7 @@ int direction = -1;
 	leftPaddle.move(0, 10);
       }
 	//Ball Physics
-	ball_traits(ball, leftPaddle, rightPaddle, direction, clock, angle, blip);
+	ball_traits(ball, leftPaddle, rightPaddle, direction, clock, angle, blip, scoreLeft, scoreRight, left, right, textLeft, textRight);
 	paddle_collision(paddleSizeY, ball, leftPaddle, rightPaddle, direction, angle, blip);
 	ai_paddle(ball, rightPaddle,  direction, clock, gameHeight);
 	//std::cout << ball.getPosition().y << std::endl;
@@ -76,14 +108,20 @@ int direction = -1;
         window.draw(leftPaddle);
 	window.draw(rightPaddle);
 	window.draw(ball);
+	window.draw(textLeft);
+	window.draw(textRight);
+	window.draw(line);
         window.display();
     }
 
     return 0;
 }
-void ball_traits(sf::RectangleShape& ball, sf::RectangleShape& leftPaddle, sf::RectangleShape& rightPaddle, int& direction, sf::Clock& clock, float& angle, sf::Sound& blip)
+void ball_traits(sf::RectangleShape& ball, sf::RectangleShape& leftPaddle, sf::RectangleShape& rightPaddle, int& direction, sf::Clock& clock, float& angle, sf::Sound& blip, int& scoreLeft, int& scoreRight, std::string& left, std::string& right, sf::Text& textLeft, sf::Text& textRight)
 {
   //So my error was I had to add asSeconds() to make the ball go faster
+
+  //temp is used for integer to character conversions
+  char temp;
   float elapsed= clock.getElapsedTime().asSeconds();
   bool stop = true;
   float ballSpeed = 1;
@@ -94,7 +132,7 @@ void ball_traits(sf::RectangleShape& ball, sf::RectangleShape& leftPaddle, sf::R
     float velocityY = scaleY * ballSpeed;
     float moveX = velocityX;
     float moveY = velocityY;
-    
+
     //Cap the speed by freezing time
     if (elapsed >= 10.f)
       {
@@ -123,26 +161,100 @@ void ball_traits(sf::RectangleShape& ball, sf::RectangleShape& leftPaddle, sf::R
 	    angle = -angle;
 	    ball.move(moveX, -moveY * elapsed);
 	    }
-	    
-    
+
+
        //prevent the ball from going out of  bounds passed the paddle
 	if (ball.getPosition().x < leftPaddle.getPosition().x || ball.getPosition().x > rightPaddle.getPosition().x)
 	 {
+        //increment scores based on which side the ball went to
+	     if(ball.getPosition().x < leftPaddle.getPosition().x)
+        {
+            scoreRight++;
+        }
+        else if(ball.getPosition().x > rightPaddle.getPosition().x)
+        {
+            scoreLeft++;
+        }
+
+        if(scoreLeft > 9)
+        {
+            //get the tens digit and convert to character
+            temp = scoreLeft/10 + '0';
+            left[0] = temp;
+            //get the ones digit and convert to character
+            temp = scoreLeft%10 + '0';
+            left[1] = temp;
+        }
+        else if(scoreLeft < 10)
+        {
+            //get the digit and convert to character
+            temp = scoreLeft + '0';
+            left[0] = '0';
+            left[1] = temp;
+        }
+        //comments for scoreLeft above apply below for scoreRight
+        if(scoreRight > 9)
+        {
+            temp = scoreRight/10 + '0';
+            right[0] = temp;
+            temp = scoreRight%10 + '0';
+            right[1] = temp;
+        }
+        else if(scoreRight < 10)
+        {
+            temp = scoreRight + '0';
+            right[0] = '0';
+            right[1] = temp;
+        }
+        //if the tens digit is 0, that means the score is less than 10, so only the ones digit is used
+        if(left[0] == '0')
+        {
+            textLeft.setString(left[1]);
+        }
+        //otherwise both digits are used
+        else
+        {
+            textLeft.setString((left));
+        }
+        //repeat comments for right
+        if(right[0] == '0')
+        {
+            textRight.setString(right[1]);
+        }
+        else
+        {
+            textRight.setString((right));
+        }
+        //reset everything
+        ball.setPosition(250, 200);
+        leftPaddle.setPosition(20, 200);
+        rightPaddle.setPosition(760,  200);
+        angle = 0;
+        direction *= -1;
+        clock.restart();
+
 	   //Press space bar to throw the ball again
-	    while (stop == true)
+	    /*while (stop == true)
 	     {
+
 	       if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space))
 	        {
-		   ball.setPosition(250, 200);
-		   leftPaddle.setPosition(20, 200);
-		   rightPaddle.setPosition(760,  200);
-		   angle = 0;
-		   direction *= -1;
-		   clock.restart();
-		   stop = false;
-		 }
-	     }
+
+
+
+
+                ball.setPosition(250, 200);
+                leftPaddle.setPosition(20, 200);
+                rightPaddle.setPosition(760,  200);
+                angle = 0;
+                direction *= -1;
+                clock.restart();
+                stop = false;
+
+            }
+	     }*/
 	 }
+
 }
 
 int paddle_collision(const int& paddleSizeY, sf::RectangleShape& ball, sf::RectangleShape& leftPaddle, sf::RectangleShape& rightPaddle, int& direction, float& angle, sf::Sound& blip)
@@ -178,7 +290,7 @@ int paddle_collision(const int& paddleSizeY, sf::RectangleShape& ball, sf::Recta
 	    if (ball.getPosition().y < rightPaddle.getPosition().y + 30)
 		{
 		    std::cout << "hitting the top right" << std::endl;
-		    angle = (rand() % 35 + 30); 
+		    angle = (rand() % 35 + 30);
 		    angle = -angle;
 		}
 	    else if (ball.getPosition().y >  rightPaddle.getPosition().y + 30 && rightPaddle.getPosition().y < rightPaddle.getPosition().y + 50)
@@ -223,6 +335,6 @@ void ai_paddle(sf::RectangleShape& ball, sf::RectangleShape& rightPaddle, int& d
 			    rightPaddle.setPosition(760, 499);
 			}
 		}
-	 
-	    //  std::cout << rightPaddle.getPosition().y << std::endl;	
+
+	    //  std::cout << rightPaddle.getPosition().y << std::endl;
 }
